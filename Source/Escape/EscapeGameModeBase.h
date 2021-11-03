20279e538fae77351666a01954197e1ad0c7fee2
Rizart Dokollari <r.dokollari@gmail.com>
2021-11-03 23:42:34 +0000

    Initialise project

---
+++ Source/Escape/EscapeGameModeBase.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_API AEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
